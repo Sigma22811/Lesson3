304f5d0c1e92a4bbde9496427477758046964ae4
Sigma22811 <amongussusbabagrila228@gmail.com>
2024-10-19 16:36:50 +0300

    Add class student to main.py #+Sigma bugfixes from sigma teacher

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 print ("Lesson3")
 
 class Student: 2 usage
-    def __init__(self,Name,surname,age):
+    def __init__(self,Name,Surname,Age):
         self.Name=Name
-        self.Name=Surname
-        self.Name=Age
+        self.Surname=Surname
+        self.Age=Age
         print("create Sucsessfull")
 
     def print_student(self):
         print("Surname:",self.Surname,"\tName",self.Name, "\tAge:",self.Age)
 
-Student1 = Student( Name: "Vitalik", Surname: "Oxygen", Age: 42)
+Student1 = Student("Vitalik","Oxygen",42)
 
-Student2 = Student( Name: "Ramzes", Surname: "Strela", Age: 25)
+Student2 = Student( "Ramzes","Strela",25)
 
 student1.print_student()
 
